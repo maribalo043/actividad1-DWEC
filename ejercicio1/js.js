c4606a93ed5abaab2312d0195c9d3689fc5ffd3b
Mario <mariobl90@educastur.es>
2023-09-27 10:17:35 +0200

    Cambios de por la mañana

---
+++ ejercicio1/js.js
@@ -0,0 +1,36 @@
+var numeroEntrante = prompt("Dame un numero entero positivo para darte su sumatorio");
+
+numeroEntrante = parseInt(numero);
+
+while(!esPositivo){
+    numeroEntrante = prompt("Te has equivocado necesito un numero entero positivo, a ser posible superior a 2");
+}
+function esEntero(numero){
+    var esEntero = false;
+    if(numero == parseInt(numero)){
+        esEntero = true;
+    }
+    return esEntero;
+}
+function esPositivo(numero){
+    var esPositivo = false;
+    if(numero>0){
+        esPositivo = true;
+    }
+    return esPositivo;
+}
+function getSumatorio(numero){
+    var sumatorio = "Sumatorio del numero " + numero;
+    var Total = 0;
+    if(esEntero == true && esPositivo == true){
+        while(numero>=0){
+            sumatorio += numero + " + ";
+            Total += numero;
+            numero--;
+        }
+    }
+    sumatorio+= " = " + Total;
+    alert(sumatorio);
+    return sumatorio;
+}
+getSumatorio(numeroEntrante);
